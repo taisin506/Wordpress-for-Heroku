70a4b59dfa32c8f6b134cf9a97cda538e09042e7
Tuhin Kanti Pal <51857187+cachecleanerjeet@users.noreply.github.com>
2020-08-05 13:01:03 +0530

    Shell Files

---
+++ config.php
@@ -0,0 +1,19 @@
+
+[users]
+<?php
+$_ENV["SHELL_HASH_PASS"]
+
+[aliases]
+
+
+ls = "ls -CvhF"
+ll = "ls -lvhF"
+
+
+[settings]
+
+
+home-directory = "."
+safe-mode-warning = true
+PS1 = "$ "
+file-upload = true
